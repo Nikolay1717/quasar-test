31608139511c144178405e445a992b9adcac5587
Nikolay1717 <kuzn1717@gmail.com>
2024-04-03 14:31:19 +0300

    project complete

---
+++ src/pages/Posts.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page>
+    <p v-for="post in posts" :key="post.id">
+      {{ post.title }}
+    </p>
+  </q-page>
+</template>
+
+<style></style>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { api } from "../boot/axios";
+
+defineOptions({
+  name: "IndexPage",
+});
+
+const posts = ref([]);
+
+onMounted(async () => {
+  const { data } = await api.get("/posts");
+  posts.value = data;
+});
+</script>
