a4db0a819260a1cf78eb1b7c3bfc03236f853f4a
Nikolay1717 <kuzn1717@gmail.com>
2024-04-03 15:10:37 +0300

    routing fix

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [
       { path: "", component: () => import("pages/IndexPage.vue") },
-      { path: "/posts", component: () => import("pages/Posts.vue") },
+      { path: "posts", component: () => import("pages/Posts.vue") },
     ],
   },
 
